5a78692e2195ab7630e87cef89e1523d244f3785
huirad <huirad@users.sf.net>
2016-02-01 21:02:26 +0000

    LSM9DS1 support: 7th step: correct ELSM9DS1OutputDataRate constants

--- sensors-service/src/lsm9ds1.h
+++ sensors-service/src/lsm9ds1.h
@@ -1,14 +1,14 @@
 /**************************************************************************
- * @brief Access library for LSM9DS1 inertial sensor 
+ * @brief Access library for LSM9DS1 inertial sensor
  *
  * @details Encapsulate I2C access to a LSM9DS1 sensor on a Linux machine
  * The LSM9DS1 from ST Microelectronics is a 6DOF inertial sensor
- * @see http://www.st.com/web/en/catalog/sense_power/FM89/SC1448/PF259998 
+ * @see http://www.st.com/web/en/catalog/sense_power/FM89/SC1448/PF259998
  *
  *
  * @author Helmut Schmidt <https://github.com/huirad>
  * @copyright Copyright (C) 2016, Helmut Schmidt
- * 
+ *
  * @license MPL-2.0 <http://spdx.org/licenses/MPL-2.0>
  *
  **************************************************************************/
@@ -23,8 +23,8 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
- 
- 
+
+
 /** Part 0: I2C device names and device addresses
  *
  */
@@ -44,31 +44,31 @@ extern "C" {
   */
 #define LSM9DS1_ADDR_1 0x6A
 #define LSM9DS1_ADDR_2 0x6B
- 
+
 /** Output data rate settings
   * See the LSM9DS1 data sheet section on CTRL_REG1_G for details
   * http://www.st.com/web/en/resource/technical/document/datasheet/DM00103319.pdf
-  * 
-  * The HPF will be not enabled. 
+  *
+  * The HPF will be not enabled.
   * Therefore the LPF2 will be not used and the LPF1 bandwidth directly depends on the ODR
-  */ 
+  */
 enum ELSM9DS1OutputDataRate
 {
     LSM9DS1_ODR_PWR_DWN     = 0x00,     //Gyro+Accel switched off
-    LSM9DS1_ODR_14_9HZ      = 0x01,     //ODR:  14.9 Hz,  LPF1 CutOff   5Hz
-    LSM9DS1_ODR_59_5HZ      = 0x02,     //ODR:  59.5 Hz,  LPF1 CutOff  19Hz
-    LSM9DS1_ODR_119HZ       = 0x03,     //ODR: 119   Hz,  LPF1 CutOff  38Hz
-    LSM9DS1_ODR_238HZ       = 0x04,     //ODR: 238   Hz,  LPF1 CutOff  76Hz
-    LSM9DS1_ODR_476HZ       = 0x05,     //ODR: 476   Hz,  LPF1 CutOff 100Hz
-    LSM9DS1_ODR_952HZ       = 0x06,     //ODR: 952   Hz,  LPF1 CutOff 100Hz    
-    
+    LSM9DS1_ODR_14_9HZ      = 0x20,     //ODR:  14.9 Hz,  LPF1 CutOff   5Hz
+    LSM9DS1_ODR_59_5HZ      = 0x40,     //ODR:  59.5 Hz,  LPF1 CutOff  19Hz
+    LSM9DS1_ODR_119HZ       = 0x60,     //ODR: 119   Hz,  LPF1 CutOff  38Hz
+    LSM9DS1_ODR_238HZ       = 0x80,     //ODR: 238   Hz,  LPF1 CutOff  76Hz
+    LSM9DS1_ODR_476HZ       = 0xA0,     //ODR: 476   Hz,  LPF1 CutOff 100Hz
+    LSM9DS1_ODR_952HZ       = 0xC0,     //ODR: 952   Hz,  LPF1 CutOff 100Hz
+
 };
 
- 
+
 /** Part 1: Functions to access the LSM9DS1
  *
  */
-  
+
 /**
  * Container for quantities which can be expressed as 3 dimensional vector.
  * The axis system is to be assumed cartesian and right-hded
@@ -76,13 +76,13 @@ enum ELSM9DS1OutputDataRate
  *   For accelerometer measurements, the unit is g
  *   For gyro measurements, the unit is degrees per seconds (deg/s)
  */
-typedef struct 
+typedef struct
 {
   float x;        /**< Vector component of the quantity along the x-axis */
   float y;        /**< Vector component of the quantity along the y-axis */
   float z;        /**< Vector component of the quantity along the Z-axis */
 } TLSM9DS1Vector3D;
-  
+
 /**
  * Initialize the LSM9DS1 access.
  * Must be called before using any of the other functions.
@@ -97,7 +97,7 @@ bool lsm9ds1_init(const char* i2c_device = LSM9DS1_I2C_DEV_DEFAULT, uint8_t i2c_
  * Release the LSM9DS1 access.
  * @return true on success.
  */
-bool lsm9ds1_deinit(); 
+bool lsm9ds1_deinit();
 
 /**
  * Read the current acceleration, angular rate and temperature from the LSM9DS1
@@ -174,7 +174,7 @@ bool lsm9ds1_stop_reader_thread();
   * LSM9DS1_UNIT_1_G: Standard gravity: 1g = 9.80665 m/(s*s)
   * @see http://en.wikipedia.org/wiki/Standard_gravity
   * @see http://www.bipm.org/utils/common/pdf/si_brochure_8_en.pdf#page=51
-  * @see http://en.wikipedia.org/wiki/Gravitational_acceleration 
+  * @see http://en.wikipedia.org/wiki/Gravitational_acceleration
   *
   * LSM9DS1_UNIT_1_RAD_IN_DEG: Angle conversion from radian to degrees
   */
@@ -184,7 +184,7 @@ bool lsm9ds1_stop_reader_thread();
 //#define M_PI 3.141592653589793238462643
 #else
 #define LSM9DS1_PI M_PI
-#endif  
+#endif
 #define LSM9DS1_UNIT_1_G 9.80665
 #define LSM9DS1_UNIT_1_RAD_IN_DEG (180.0/MPU6050_PI)
 
@@ -192,7 +192,7 @@ bool lsm9ds1_stop_reader_thread();
  * Get system timestamp
  * @return returns a system timestamp in ms (milliseconds) derived from clock_gettime(CLOCK_MONOTONIC);
  */
-uint64_t lsm9ds1_get_timestamp(); 
+uint64_t lsm9ds1_get_timestamp();
 
 
 #ifdef __cplusplus
